9a5e19047690efd35ec06d9ab39b1db6768e4731
Klaus Lehner <172195+klu2@users.noreply.github.com>
2022-04-27 10:43:30 +0300

    update version

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 
 description = "Collection of icons for use in architecture diagrams, documentation and training"
 group = "io.cloudflight.architectureicons"
-version = "0.1.0"
+version = "0.2.0"
 
 repositories {
     mavenCentral()
